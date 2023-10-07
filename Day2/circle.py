b95dbf08f79b4349f2e255f932f72a3926bab0c7
sachin2156 <sachinsingh2156@gmail.com>
2023-10-07 11:05:17 +0530

    day 2 question no. 7 soved

---
+++ Day2/circle.py
@@ -0,0 +1,6 @@
+radius = float(input("Enter Radius of Circle : "))
+area = (22/7)*radius**2  #formula for area of circle
+circumference = 2 * (22/7) * radius
+
+print ("Area of the circle is ", round(area,3))   #round off the answer to three decimal places.
+print ("Circumference of the circle is", round(circumference,3) )
